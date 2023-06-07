b6897a6a695020f182757cb150d4a9acf48caa9a
marc <marcsmithr@gmail.com>
2023-06-07 16:41:33 -0500

    added logic and css to display selected nav icon

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import './NavBar.css'
@@ -7,7 +7,33 @@ import './NavBar.css'
 const NavBar = () => {
   const [selectedIcon, setSelectedIcon] = useState("home")
 
+  let homeId = ""
+  let causeId = ""
+  let eventId = ""
+  let groupId = ""
+  let worldId = ""
 
+  switch(selectedIcon){
+    case "home":
+      homeId = "selected"
+      break
+    case "causes":
+      causeId = "selected"
+      break
+    case "events":
+      eventId = "selected"
+      break
+    case "groups":
+      groupId = "selected"
+      break
+    case "world":
+      worldId = "selected"
+      break
+  }
+
+  useEffect(()=>{
+    console.log(selectedIcon)
+  }, [selectedIcon])
   return (
     <nav>
       <div className='nav-home-search-column'>
@@ -28,24 +54,24 @@ const NavBar = () => {
       </div>
       <div className='nav-icon-column'>
 
-        <div className='nav-icon-container' onClick={setSelectedIcon("home")}>
+        <div className='nav-icon-container' onClick={()=>setSelectedIcon("home")} id={homeId}>
           <i class="fa-solid fa-house"></i>
         </div>
 
-        <div className='nav-icon-container' onClick={setSelectedIcon("causes")}>
+        <div className='nav-icon-container' onClick={()=>setSelectedIcon("causes")} id={causeId}>
          <i class="fa-solid fa-trowel"></i>
         </div>
 
-        <div className='nav-icon-container' onClick={setSelectedIcon("groups")}>
+        <div className='nav-icon-container' onClick={()=>setSelectedIcon("groups")} id={groupId}>
           <i class="fa-solid fa-people-group"></i>
         </div>
 
 
-        <div className='nav-icon-container' onClick={setSelectedIcon("events")}>
+        <div className='nav-icon-container' onClick={()=>setSelectedIcon("events")} id={eventId}>
           <i class="fa-regular fa-calendar-days"></i>
         </div>
 
-        <div className='nav-icon-container' onClick={setSelectedIcon("world")}>
+        <div className='nav-icon-container' onClick={()=>setSelectedIcon("world")} id={worldId}>
           <i class="fa-solid fa-globe"></i>
         </div>
 
