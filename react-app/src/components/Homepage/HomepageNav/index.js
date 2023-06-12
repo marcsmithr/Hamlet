3acd2e91cfe398b54ade7b9e40a45e9486df02db
marc <marcsmithr@gmail.com>
2023-06-12 18:57:21 -0500

    added nav column styling

--- react-app/src/components/Homepage/HomepageNav/index.js
+++ react-app/src/components/Homepage/HomepageNav/index.js
@@ -4,19 +4,19 @@ export default function HomepageNav(){
     return(
         <>
             <div className="home-page-nav">
-                <div className="home-page-nav-causes">
+                <div className="home-page-nav-el">
                     <i class="fa-solid fa-trowel-bricks nav-icon"></i>
                     <span className='nav-name'>Causes</span>
                 </div>
-                <div className="home-page-nav-groups">
+                <div className="home-page-nav-el">
                     <i class="fa-solid fa-users-line nav-icon"></i>
                     <span className='nav-name'>Groups</span>
                 </div>
-                <div className="home-page-nav-events">
+                <div className="home-page-nav-el">
                     <i class="fa-solid fa-calendar-days nav-icon"></i>
                     <span className='nav-name'>Events</span>
                 </div>
-                <div className="home-page-nav-world">
+                <div className="home-page-nav-el">
                     <i class="fa-solid fa-earth-americas nav-icon"></i>
                     <span className='nav-name'>World</span>
                 </div>
