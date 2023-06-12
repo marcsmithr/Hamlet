03a87072cf9bb32d916a91809623bb2c19be4a8a
marc <marcsmithr@gmail.com>
2023-06-12 18:41:38 -0500

    styled left nav column

--- react-app/src/components/Homepage/HomepageNav/index.js
+++ react-app/src/components/Homepage/HomepageNav/index.js
@@ -5,16 +5,20 @@ export default function HomepageNav(){
         <>
             <div className="home-page-nav">
                 <div className="home-page-nav-causes">
-
+                    <i class="fa-solid fa-trowel-bricks nav-icon"></i>
+                    <span className='nav-name'>Causes</span>
                 </div>
                 <div className="home-page-nav-groups">
-
+                    <i class="fa-solid fa-users-line nav-icon"></i>
+                    <span className='nav-name'>Groups</span>
                 </div>
                 <div className="home-page-nav-events">
-
+                    <i class="fa-solid fa-calendar-days nav-icon"></i>
+                    <span className='nav-name'>Events</span>
                 </div>
                 <div className="home-page-nav-world">
-
+                    <i class="fa-solid fa-earth-americas nav-icon"></i>
+                    <span className='nav-name'>World</span>
                 </div>
             </div>
         </>
