9848e9c17ef0c7cc48d91bbe73eba085ef1f9bed
marc <marcsmithr@gmail.com>
2023-06-13 15:28:26 -0500

    added logic to switch between login and signup

--- react-app/src/components/AuthPage/index.js
+++ react-app/src/components/AuthPage/index.js
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import LoginForm from "../auth/LoginForm"
+import SignUpForm from "../auth/SignUpForm"
+import './index.css'
 
 function AuthPage(){
+    const [isLogin, setIsLogin] = useState(true)
+
+    const loginFormId = isLogin? "" : "hidden"
+    const signUpFormId = !isLogin? "" : "hidden"
 
     return(
         <>
@@ -10,8 +17,15 @@ function AuthPage(){
                     <span>Hamlet</span>
                 </div>
             </div>
-            <div>
+            <div id={loginFormId}>
                 <LoginForm/>
+                <span>New to Hamlet?</span>
+                <span onClick={()=>setIsLogin(false)}>Sign Up</span>
+            </div>
+            <div id={signUpFormId}>
+                <SignUpForm/>
+                <span>Already have an account?</span>
+                <span onClick={()=>setIsLogin(true)}>Log In</span>
             </div>
         </>
     )
