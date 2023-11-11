ed2dacf184b89df7dd760339709b7333401ec68e
marc <marcsmithr@gmail.com>
2023-11-11 10:09:39 -0600

    began profile button

---
+++ react-app/src/components/NavBar/ProfileButton.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import { Link, NavLink, useHistory } from "react-router-dom";
+import './ProfileButton.css'
+
+function ProfileButton(){
+    const [showMenu, setshowMenu] = useState(false);
+}
